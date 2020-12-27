5254c46bcdf1b03891325462466bd16bfb11da9d
Alexander Drach <alexander-drach@mail.ru>
2020-12-27 16:31:21 +0600

    add ShopItemFunc

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,20 @@
+export function ShopItemFunc(props) {
+    const { item } = props;
+    item.price = (item.price).toFixed(2);
+    return (
+        <div class="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div class="description">
+                {item.descriptionFull}
+            </div>
+            <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+            <div class="divider"></div>
+            <div class="purchase-info">
+                <div class="price">{item.currency} {item.price}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ShopItemFunc } from './components/ShopItemFunc';
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className="container">
+    <div className="background-element">
+    </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemFunc item={item} />
+    </div>
+  </div>,
   document.getElementById('root')
 );
 
